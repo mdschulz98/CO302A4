cc504f676c00c291ffd155aa2d1f08e3ca535ac3
Matthew David Schulz <46827170+mdschulz98@users.noreply.github.com>
2019-05-15 13:42:11 -0600

    heights

--- functions.js
+++ functions.js
@@ -2,6 +2,10 @@ function adjustHeight() {
     document.getElementById('story_text').style.height = document.defaultView.getComputedStyle(document.getElementById('option1'), "").getPropertyValue("height");
 }
 
+function adjust2() {
+  document.getElementById('option2').style.height = document.defaultView.getComputedStyle(document.getElementById('option1'), "").getPropertyValue("height");
+}
+
 function topFunction() {
     document.body.scrollTop = 0; // For Safari
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
